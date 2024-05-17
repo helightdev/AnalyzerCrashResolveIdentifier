fb000adffc4fc749b02f769479d507597f54d909
helight <contact@helight.dev>
2024-05-18 00:14:57 +0200

    Update for new issue

--- lib/controller.dart
+++ lib/controller.dart
@@ -3,9 +3,14 @@ import 'package:macro_test/macro_test.dart';
 @ControllerMacro()
 class MyControllerText {
   @CustomAnnotation()
-  void main() {}
+  @refAnnotation
+  void main() {
+    
+  }
 }
 
 class CustomAnnotation {
   const CustomAnnotation();
 }
+
+const refAnnotation = CustomAnnotation();
